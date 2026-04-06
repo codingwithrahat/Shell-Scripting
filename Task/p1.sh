67a5d06c40e45da4b0f6e6d5da50dc0e7a00f7a1
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-04-06 21:40:11 +0600

    Add FizzBuzz script in p1.sh

---
+++ Task/p1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+n=$1
+
+for i in $(seq 1 1 $n)
+do 
+    if [ $((i % 3)) -eq 0 ] && [ $((i % 5)) -eq 0 ]
+    then
+        echo "FizzBuzz"
+    elif [ $((i % 3)) -eq 0 ]
+    then
+        echo "Fizz"
+    elif [ $((i % 5)) -eq 0 ]
+    then
+        echo "Buzz"
+    else
+        echo "$i"
+    fi 
+done
+
